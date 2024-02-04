9de3a6b4628a84a70f73e92ddde35777f5849b12
satin <satin3737@gmail.com>
2024-02-04 16:40:12 +0200

    firebase protection test

--- screens/WelcomeScreen/index.js
+++ screens/WelcomeScreen/index.js
@@ -1,11 +1,29 @@
+import axios from 'axios';
+import {useContext, useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
+import {AuthContext} from '../../store/AuthContext';
 import styles from './style';
 
 const WelcomeScreen = () => {
+    const [fetchedText, setFetchedText] = useState('');
+    const {token} = useContext(AuthContext);
+
+    const testProtection = async () => {
+        const res = await axios.get(
+            `https://auth-native-app-f03fc-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=${token}`
+        );
+        setFetchedText(res.data);
+    };
+
+    useEffect(() => {
+        testProtection();
+    }, []);
+
     return (
         <View style={styles.rootContainer}>
             <Text style={styles.title}>Welcome!</Text>
             <Text>You authenticated successfully!</Text>
+            <Text>{fetchedText}</Text>
         </View>
     );
 };
